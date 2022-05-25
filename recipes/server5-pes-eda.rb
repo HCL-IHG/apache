d757c616ac0ed480ff3d965dddff7c84ef3b1144
HCL-IHG <hcl.ihg@gmail.com>
2022-05-25 12:42:53 +0000

    recipe updated with new files

--- recipes/server5-pes-eda.rb
+++ recipes/server5-pes-eda.rb
@@ -1,20 +1,21 @@
 environment = node.chef_environment
+environment = 'Production'
 edakafkasvrs = node['edakafka'][environment]
 template '/etc/logstash/conf.d/pes-eda.conf' do
   source 'server5-PES.conf.erb'
   owner 'root'
   group 'root'
   mode '0640'
-  notifies :restart, 'service[logstash]'
-  variables(elasticsearchsvr5_cluster1: node['elasticsearch5'][environment]['pes-eda'], edakafkasvr: edakafkasvrs)
+#  notifies :restart, 'service[logstash]'
+  variables(edakafkasvr: edakafkasvrs)
   not_if { node['Datacenter'] == 'SJCD1' }
 end
 
 template "#{node['logstash']['dir']}/elasticsearch-template5-pes-eda.json" do
   source 'server5-PES.template-json.erb'
-  owner 'logstash'
-  group 'logstash'
+#  owner 'logstash'
+#  group 'logstash'
   mode '0644'
-  notifies :restart, 'service[logstash]'
+#  notifies :restart, 'service[logstash]'
   not_if { node['Datacenter'] == 'SJCD1' }
 end
